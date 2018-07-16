930848438f18cf7934cb25cc428a35655c715fd9
ehnawh <ehnawh@gmail.com>
2018-07-16 18:09:51 +0900

    fix stylelint autofix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,19 +4,18 @@ module.exports = {
   // The standard config based on a handful of CSS style guides
   // https://github.com/stylelint/stylelint-config-standard
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-recommended",
-    "stylelint-config-recommended-scss"
-    // 'stylelint-config-prettier'
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss'
   ],
 
   plugins: [
     // stylelint plugin to sort CSS rules content with specified order
     // https://github.com/hudochenkov/stylelint-order
-    "stylelint-order",
-    "stylelint-scss"
+    'stylelint-order',
+    'stylelint-scss'
   ],
   rules: {
-    "no-empty-source": null
+    'no-empty-source': null,
+    'string-quotes': 'single'
   }
 };
