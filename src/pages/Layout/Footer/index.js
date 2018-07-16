a24768447043d81120fc2d546c90c18ca0d6dc77
EunseokGo <gomonk3037@gmail.com>
2018-07-16 21:17:42 +0900

    Add footer content

--- src/pages/Layout/Footer/index.js
+++ src/pages/Layout/Footer/index.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import classNames from 'classnames/bind';
 
-class Footer extends Component {
-  render() {
-    return <footer>{this.props.children}</footer>;
-  }
-}
+import styles from './style.scss';
+
+import logo from '~/assets/images/logo/footer.svg';
+
+const cx = classNames.bind(styles);
+
+const Footer = () => {
+  return (
+    <footer className={cx('footer')}>
+      <div className="container">
+        <img className={cx('logo')} src={logo} alt="logo" />
+        <div>
+          <a
+            className="d-block text-light"
+            href="mailto:support@bcventures.io"
+            title="support mail"
+          >
+            support@bcventures.io
+          </a>
+          <p className="m-0">Ⓒ 2018 GXC World Pte Ltd</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
