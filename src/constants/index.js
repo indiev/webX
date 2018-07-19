8ad654114b061a587b7699068246dea9306768b3
ehnawh <ehnawh@gmail.com>
2018-07-19 22:27:37 +0900

    add ga and color to Button component

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,12 @@ const GA_ID = process.env.GA_ID || '';
 const IMAGE_PATH = 'assets/images';
 const VIDEO_PATH = 'assets/videos';
 
-export { API_ENDPOINT, GA_ID, IMAGE_PATH, VIDEO_PATH };
+const COMPONENT_COLOR = {
+  BLUE: 'primary',
+  GREEN: 'success',
+  RED: 'danger',
+  YELLOW: 'warning',
+  GRAY: 'secondary'
+};
+
+export { API_ENDPOINT, GA_ID, IMAGE_PATH, VIDEO_PATH, COMPONENT_COLOR };
