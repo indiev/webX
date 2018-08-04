90a4548b1ca6c7a646fb014b5ffb25f445390498
EunseokGo <gomonk3037@gmail.com>
2018-08-05 00:26:08 +0900

    Remove unrelated codes in Layout

--- src/pages/Layout/Main/index.js
+++ src/pages/Layout/Main/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Routes from './Routes';
 
 class Main extends Component {
   render() {
     return (
-      <main className="flex-fill d-flex align-items-center" role="main">
-        <Routes />
+      <main className="flex-fill" role="main">
+        {this.props.children}
       </main>
     );
   }
