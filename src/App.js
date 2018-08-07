dcfd4f33663d7e2e186ed831e6ed1b12a05e60de
ehnawh <ehnawh@gmail.com>
2018-08-07 20:57:09 +0900

    remove legacy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import GA from 'react-ga';
 import { Header, Main, Footer } from './pages/Layout';
 import trackGA from './utils/ga/trackGA';
-
-import { GA_ID } from '~/constants';
-
-GA.initialize(GA_ID);
 
 @withRouter
 @trackGA
