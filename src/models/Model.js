767c3ae231aa3902f86f3923939fa43a7012cfee
ehnawh <ehnawh@gmail.com>
2018-08-07 20:57:27 +0900

    add Model

---
+++ src/models/Model.js
@@ -0,0 +1,8 @@
+class Model {
+  constructor(params) {
+    this.createdDate = this.param.createdDates || new Date();
+    this.updatedDate = this.prams.updatedDate || new Date();
+  }
+}
+
+export default Model;
