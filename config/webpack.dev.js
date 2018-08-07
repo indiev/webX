6d064b89e1c70a9652682d7ce78ffc154805e225
EunseokGo <gomonk3037@gmail.com>
2018-08-07 18:17:24 +0900

    Fix double quote to single in config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,50 +1,50 @@
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ignoredFiles = require("react-dev-utils/ignoredFiles");
-const common = require("./webpack.common");
-const paths = require("./paths");
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ignoredFiles = require('react-dev-utils/ignoredFiles');
+const common = require('./webpack.common');
+const paths = require('./paths');
 
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = 'development';
 
 const regexpStyle = /\.(css|less|styl|scss|sass|sss)$/;
 
 const cssModule = ({ modules = false }) => [
   {
-    loader: require.resolve("css-loader"),
+    loader: require.resolve('css-loader'),
     options: {
       importLoaders: 2,
       camelCase: true,
       modules,
-      localIdentName: "[name]-[local]-[hash:base64:5]"
+      localIdentName: '[name]-[local]-[hash:base64:5]'
     }
   },
   {
-    loader: require.resolve("postcss-loader"),
+    loader: require.resolve('postcss-loader'),
     options: {
-      ident: "postcss",
+      ident: 'postcss',
       config: {
         ctx: {
           autoprefixer: {
             browsers: [
-              ">1%",
-              "last 4 versions",
-              "Firefox ESR",
-              "not ie < 10" // React doesn't support IE9 anyway
+              '>1%',
+              'last 4 versions',
+              'Firefox ESR',
+              'not ie < 10' // React doesn't support IE9 anyway
             ],
-            flexbox: "no-2009"
+            flexbox: 'no-2009'
           }
         }
       },
       plugins: () => [
-        require("postcss-flexbugs-fixes"),
-        require("precss"),
-        require("autoprefixer")
+        require('postcss-flexbugs-fixes'),
+        require('precss'),
+        require('autoprefixer')
       ]
     }
   },
   {
-    loader: require.resolve("sass-loader"),
+    loader: require.resolve('sass-loader'),
     options: {
       sourceMap: true
     }
@@ -52,10 +52,10 @@ const cssModule = ({ modules = false }) => [
 ];
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: 'development',
   bail: true,
   watch: true,
-  devtool: "cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     hot: true,
     contentBase: paths.appPublic,
@@ -63,24 +63,24 @@ module.exports = merge(common, {
     watchOptions: {
       ignored: ignoredFiles(paths.appSrc)
     },
-    host: process.env.HOST || "localhost",
+    host: process.env.HOST || 'localhost',
     port: process.env.PORT || 3000,
-    publicPath: "/",
+    publicPath: '/',
     historyApiFallback: true,
     disableHostCheck: true,
-    clientLogLevel: "warning"
+    clientLogLevel: 'warning'
   },
   module: {
     rules: [
       {
         test: regexpStyle,
         exclude: paths.appGlobalStyles,
-        use: [require.resolve("style-loader"), ...cssModule({ modules: true })]
+        use: [require.resolve('style-loader'), ...cssModule({ modules: true })]
       },
       {
         test: regexpStyle,
         include: paths.appGlobalStyles,
-        use: [require.resolve("style-loader"), ...cssModule({ modules: false })]
+        use: [require.resolve('style-loader'), ...cssModule({ modules: false })]
       }
     ]
   },
@@ -96,7 +96,7 @@ module.exports = merge(common, {
   optimization: {
     namedModules: true,
     namedChunks: true,
-    nodeEnv: "development",
+    nodeEnv: 'development',
     minimize: false
   },
   performance: {
