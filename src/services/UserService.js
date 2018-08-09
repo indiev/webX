716475abb0f8cf56199fd36f4e443f5a7a11b5e4
EunseokGo <gomonk3037@gmail.com>
2018-08-09 18:03:05 +0900

    Add user api to UserService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,13 +1,35 @@
 import { API_ENDPOINT } from '~/constants';
+import request from '~/utils/request';
+
 class UserService {
   constructor() {
-    this.baseUrl = `${API_ENDPOINT}/users`;
+    this.url = `${API_ENDPOINT}/users`;
   }
 
   async getByUsername(username) {
-    const getByUsernameUrl = `${this.baseUrl}?username=${username}`;
-    const result = await fetch(getByUsernameUrl);
+    const result = await request({ url: this.url, params: { username } });
     return result;
+  }
+
+  async getCurrentUser() {
+    const { user } = await request({ url: `${this.url}/profile` });
+    return user;
+  }
+
+  async login(data) {
+    return await request({
+      url: `${this.url}/login`,
+      method: 'post',
+      data
+    });
+  }
+
+  async register(data) {
+    return await request({
+      url: `${this.url}/register`,
+      method: 'post',
+      data
+    });
   }
 }
 
